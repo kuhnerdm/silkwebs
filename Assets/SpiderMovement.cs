8f8f12ffb67fb9dda8812f62bfa18b2c3a97589b
kuhnerdm <kuhnerdm@rose-hulman.edu>
2017-04-17 13:31:54 -0400

    Added new movement files and removed caching from project

---
+++ Assets/SpiderMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour {
+
+	public float maxVel;
+	public float jumpVel;
+	public float easing;
+	public float epsilon;
+
+	void FixedUpdate() {
+		if(Input.GetKey(KeyCode.RightArrow) && !objectToRight()) {
+			Vector3 oldVel = GetComponent<Rigidbody>().velocity;
+			Vector3 newVel = new Vector3(oldVel.x, oldVel.y, Mathf.Lerp(oldVel.z, maxVel, easing));
+			GetComponent<Rigidbody>().velocity = newVel;
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow) && !objectToLeft()) {
+			Vector3 oldVel = GetComponent<Rigidbody>().velocity;
+			Vector3 newVel = new Vector3(oldVel.x, oldVel.y, Mathf.Lerp(oldVel.z, -maxVel, easing));
+			GetComponent<Rigidbody>().velocity = newVel;
+		}
+
+		if(Input.GetKey(KeyCode.Space) && Mathf.Abs(GetComponent<Rigidbody>().velocity.y) < epsilon && isOnGround()) {
+			Vector3 oldVel = GetComponent<Rigidbody>().velocity;
+			Vector3 newVel = new Vector3(oldVel.x, jumpVel, oldVel.z);
+			GetComponent<Rigidbody>().velocity = newVel;
+		}
+
+	}
+
+	bool isOnGround() {
+
+		Vector3 toLeftCorner = new Vector3(0f, -GetComponent<Collider>().bounds.extents.y, -GetComponent<Collider>().bounds.extents.z + 0.2f);
+		Vector3 toRightCorner = new Vector3(0f, -GetComponent<Collider>().bounds.extents.y, GetComponent<Collider>().bounds.extents.z - 0.2f);
+
+		bool down = Physics.Raycast(transform.position, -Vector3.up, GetComponent<Collider>().bounds.extents.y + 0.1f);
+		bool diagLeft = Physics.Raycast(transform.position, toLeftCorner, toLeftCorner.magnitude + 0.1f);
+		bool diagRight = Physics.Raycast(transform.position, toRightCorner, toRightCorner.magnitude + 0.1f);
+
+		return down || diagLeft || diagRight;
+	}
+
+	bool objectToRight() {
+		Vector3 toTopCorner = new Vector3(0f, GetComponent<Collider>().bounds.extents.y, GetComponent<Collider>().bounds.extents.z);
+		Vector3 toBottomCorner = new Vector3(0f, -GetComponent<Collider>().bounds.extents.y + 0.2f, GetComponent<Collider>().bounds.extents.z);
+
+		bool right = Physics.Raycast(transform.position, Vector3.forward, GetComponent<Collider>().bounds.extents.z + 0.1f);
+		bool diagUp = Physics.Raycast(transform.position, toTopCorner, toTopCorner.magnitude + 0.1f);
+		bool diagDown = Physics.Raycast(transform.position, toBottomCorner, toBottomCorner.magnitude + 0.1f);
+
+		return right || diagUp || diagDown;
+	}
+
+	bool objectToLeft() {
+		Vector3 toTopCorner = new Vector3(0f, GetComponent<Collider>().bounds.extents.y, -GetComponent<Collider>().bounds.extents.z);
+		Vector3 toBottomCorner = new Vector3(0f, -GetComponent<Collider>().bounds.extents.y + 0.2f, -GetComponent<Collider>().bounds.extents.z);
+
+		bool left = Physics.Raycast(transform.position, Vector3.back, GetComponent<Collider>().bounds.extents.z + 0.1f);
+		bool diagUp = Physics.Raycast(transform.position, toTopCorner, toTopCorner.magnitude + 0.1f);
+		bool diagDown = Physics.Raycast(transform.position, toBottomCorner, toBottomCorner.magnitude + 0.1f);
+
+		return left || diagUp || diagDown;
+	}
+
+}
