0752f2a3ac174ed3907eb5e239d07f10d48a2eeb
palmmv <palmmv@rose-hulman.edu>
2017-05-06 19:50:35 -0400

    Termites almost done.

--- Assets/TermiteMovement.cs
+++ Assets/TermiteMovement.cs
@@ -9,24 +9,24 @@ public class TermiteMovement : MonoBehaviour {
     private float direction = -1f;
 
     //for checking platform collisions
-    private float xExtent = 0.8f;
-    private float yExtent = 0.5f;
-    private int allExceptSpiderAndRockMask = ~(3 << 8);
+    private float xExtent = 0.45f;
+    private float yExtent = 0.2f;
+    private float xOffset = .08f;
+    private int allExceptTermiteSpiderAndRockMask = ~((1 << 10) | (1 << 9) | (1 << 8));
 
     // Use this for initialization
     void Start () {
-		
-	}
+        //this.GetComponent<BoxCollider2D>().size.x / 2;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 pos = transform.position;
-        pos.x += direction * (speed + Time.deltaTime);
         if (collidewithPlatform()) 
         {
             direction = -direction;
-            pos.x += 2 * (direction * (speed + Time.deltaTime));
         }
+        pos.x += direction * (speed + Time.deltaTime);
         transform.position = pos;
 	}
 
@@ -40,12 +40,12 @@ public class TermiteMovement : MonoBehaviour {
     bool objectToRight()
     {
         Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-        Vector2 toTopCorner = new Vector2(xExtent, yExtent);
-        Vector2 toBottomCorner = new Vector2(xExtent, -yExtent * 0.4f);
+        Vector2 toTopCorner = new Vector2(xExtent + xOffset, yExtent);
+        Vector2 toBottomCorner = new Vector2(xExtent + xOffset, -yExtent * 0.4f);
 
-        bool right = Physics2D.Raycast(pos, Vector2.right, xExtent * 1.5f, allExceptSpiderAndRockMask);
-        bool diagUp = Physics2D.Raycast(pos, toTopCorner, toTopCorner.magnitude, allExceptSpiderAndRockMask);
-        bool diagDown = Physics2D.Raycast(pos, toBottomCorner, toBottomCorner.magnitude, allExceptSpiderAndRockMask);
+        bool right = Physics2D.Raycast(pos, Vector2.right, 0, allExceptTermiteSpiderAndRockMask);
+        bool diagUp = Physics2D.Raycast(pos, toTopCorner, toTopCorner.magnitude, allExceptTermiteSpiderAndRockMask);
+        bool diagDown = Physics2D.Raycast(pos, toBottomCorner, toBottomCorner.magnitude, allExceptTermiteSpiderAndRockMask);
         return right || diagUp || diagDown;
     }
 
@@ -55,9 +55,16 @@ public class TermiteMovement : MonoBehaviour {
         Vector2 toTopCorner = new Vector2(-xExtent, yExtent);
         Vector2 toBottomCorner = new Vector2(-xExtent, -yExtent * 0.4f);
 
-        bool left = Physics2D.Raycast(pos, Vector2.left, xExtent * 1.5f, allExceptSpiderAndRockMask);
-        bool diagUp = Physics2D.Raycast(pos, toTopCorner, toTopCorner.magnitude, allExceptSpiderAndRockMask);
-        bool diagDown = Physics2D.Raycast(pos, toBottomCorner, toBottomCorner.magnitude, allExceptSpiderAndRockMask);
+        bool left = Physics2D.Raycast(pos, Vector2.left, 0, allExceptTermiteSpiderAndRockMask);
+        bool diagUp = Physics2D.Raycast(pos, toTopCorner, toTopCorner.magnitude, allExceptTermiteSpiderAndRockMask);
+        bool diagDown = Physics2D.Raycast(pos, toBottomCorner, toBottomCorner.magnitude, allExceptTermiteSpiderAndRockMask);
         return left || diagUp || diagDown;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collwith = coll.gameObject;
+        if (collwith.tag == "Spider")
+            print("Termite hits Spider.");
     }
 }
