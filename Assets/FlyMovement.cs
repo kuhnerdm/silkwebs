3b72d00af6a3162bbb90f9df44efe2b5040a0c3d
palmmv <palmmv@rose-hulman.edu>
2017-05-06 20:47:41 -0400

    Flies done.

--- Assets/FlyMovement.cs
+++ Assets/FlyMovement.cs
@@ -4,11 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlyMovement : MonoBehaviour {
-    private float sightradius = 5f;
+    private float sightradius = 3f;
     private bool fleeing = false;
     private float speed = 3f;
     private GameObject spider;
-    private Transform thisTransform;
 
 	// Use this for initialization
 	void Start () {
@@ -24,16 +23,23 @@ public class FlyMovement : MonoBehaviour {
         }
         else
         {
-            Vector3 direction = thisTransform.position - spider.GetComponent<Transform>().position;
+            Vector3 direction = transform.position - spider.transform.position;
             direction.Normalize();
-            transform.position = Vector3.MoveTowards(transform.position, direction * sightradius, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, spider.transform.position, -speed * Time.deltaTime);
             //TODO get angle at which spider approaches, then fly away at that angle.
         }
 	}
 
     private bool seesSpider()
     {
-        float distance = Vector3.Distance(thisTransform.position, spider.transform.position);
+        float distance = Vector3.Distance(transform.position, spider.transform.position);
         return distance <= sightradius;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject collwith = coll.gameObject;
+        if (collwith.tag == "Spider")
+            Debug.Log("Fly hits Spider.", this);
     }
 }
