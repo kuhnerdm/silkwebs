5a8298e0374caa8a18d13fb2c3d630b20eaae27a
tepleyna <tepleyna@gmail.com>
2017-05-09 10:55:45 -0400

    try try

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    public float offset;
+    static public FollowCam S;
+    
+
+    public bool _____________;
+
+    public GameObject poi; //thing camera follows
+		 //usually/always spider
+
+    public float camZ;
+
+	// Use this for initialization
+	void Start () {
+		S = this;
+		camZ = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (poi == null) return;
+
+		Vector3 destination = poi.transform.position;
+		destination.z = camZ + offset;
+		transform.position = destination;
+	}
+}
