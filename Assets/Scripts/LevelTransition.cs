89b1b941c99732c475fc3566c8fd02cc66373a13
tepleyna <tepleyna@gmail.com>
2017-05-17 11:48:29 -0400

    transfer to black on exit level (first example leve).. Next: add to all

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelTransition : MonoBehaviour {
+    public float transRate = 1f;
+    public GameObject screen;
+    //private GameObject mesh;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    void OnCollisionEnter2D(Collision2D coll)
+    private float alpha = 0.0f;
+    private bool isFade = false;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        GameObject collwith = coll.gameObject;
-        if (collwith.tag == "Spider")
+        
+        Debug.Log("I update?" + isFade, this);
+        if (isFade)
+        {
+            if (alpha >= 1f)
+            {
+                isFade = false;
+            }
+            else
+            {
+                Debug.Log("alpha " + alpha, this);
+                alpha += transRate * (alpha + transRate);
+                if (alpha > 1f)
+                {
+                    alpha = 1f;
+                }
+            }
+            Color c = screen.GetComponent<Renderer>().material.color;
+            c.a = alpha;
+            screen.GetComponent<Renderer>().material.color = c;
+        }
+
+        if (alpha >= 1) { 
+
             if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
             {
                 Debug.Log("Level end", this);
@@ -30,11 +54,18 @@ public class LevelTransition : MonoBehaviour {
                 Debug.Log("Level end, resetting.", this);
                 SceneManager.LoadScene(0, LoadSceneMode.Single);
             }
-        else
-	{
-		Debug.Log("thing was not a spider", this);
-		// do something?
+        }   
+    
+
 	}
-            
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject collwith = coll.gameObject;
+        if (collwith.tag == "Spider")
+        {
+            isFade = true;
+        }
     }
+    
 }
