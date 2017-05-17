77f19bba4e2745b7116099624c105b48a8d2f5ea
Patrick Sullivan <sullivpc@rose-hulman.edu>
2017-05-17 17:09:05 -0400

    Add silk.

---
+++ Assets/Scripts/SilkThreadBehavior.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SilkThreadBehavior : MonoBehaviour {
+
+    public GameObject silkNodePrefab;
+
+    /// <summary>
+    /// Distance between silk thread nodes.
+    /// </summary>
+    public float nodeDistance = 0.1f;
+
+    /// <summary>
+    /// List of silk links that make up the thread
+    /// </summary>
+    public List<GameObject> threadNodes;
+
+    [HideInInspector]
+    GameObject attachedObject;
+
+    #region Unity methods
+
+    /// Use this for initialization
+    void Start () {
+    }
+
+    /// Update is called once per frame
+    void Update () {
+        RenderLine();
+    }
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Perform the initial attachment of the silk between the spider and some attachment point.
+    /// </summary>
+    public void InitialAttach(GameObject spider, GameObject attachToObject, Vector2 attachmentPoint)
+    {
+        attachedObject = attachToObject;
+        threadNodes = new List<GameObject>();
+
+        // Create the first node at the attachment point. Link the attachment point to the first node.
+        GameObject firstNode = Instantiate(silkNodePrefab, attachmentPoint, Quaternion.identity) as GameObject;
+        firstNode.transform.parent = ThisThread.transform;
+        threadNodes.Add(firstNode);
+        attachedObject.GetComponent<FixedJoint2D>().enabled = true;
+        attachedObject.GetComponent<FixedJoint2D>().connectedBody = firstNode.GetComponent<Rigidbody2D>();
+
+        Vector2 attachmentToSpider = (Vector2)spider.transform.position - attachmentPoint;
+        float attachmentToSpiderDist = attachmentToSpider.magnitude;
+        attachmentToSpider.Normalize();
+
+        GameObject prevNode = firstNode;
+        GameObject nextNode = null;
+
+        for (float attachmentToNextDist = nodeDistance; 
+             attachmentToNextDist < attachmentToSpiderDist;
+             attachmentToNextDist += nodeDistance)
+        {
+            Vector2 toNextPos = attachmentToSpider * attachmentToNextDist;
+            Vector2 nextNodePos = attachmentPoint + toNextPos;
+            nextNode = Instantiate(silkNodePrefab, nextNodePos, Quaternion.identity) as GameObject;
+            nextNode.transform.parent = ThisThread.transform;
+            threadNodes.Add(nextNode);
+            prevNode.GetComponent<HingeJoint2D>().connectedBody = nextNode.GetComponent<Rigidbody2D>();
+            prevNode = nextNode;
+        }
+
+        GameObject lastNode = (nextNode == null) ? firstNode : nextNode;
+        lastNode.GetComponent<HingeJoint2D>().connectedBody = spider.GetComponent<Rigidbody2D>();
+    }
+
+    #endregion
+
+    #region Public properties
+
+
+    #endregion
+
+    #region Private properties
+
+    /// <summary>
+    /// The silk thread to which this script is attached.
+    /// </summary>
+    private GameObject ThisThread { get { return transform.gameObject; } }
+
+    /// <summary>
+    /// The silk node at the front end of the thread, the end that is not extended by <see cref="Extend"/>
+    /// </summary>
+    private GameObject FirstNode { get { return threadNodes[0]; } }
+
+    /// <summary>
+    /// The silk node at the back end of the thread, the end of the thread that is extended by <see cref="Extend"/> and
+    /// that is initially attached to the spider
+    /// </summary>
+    private GameObject LastNode { get { return threadNodes[threadNodes.Count - 1]; } }
+
+    private LineRenderer LineRenderer { get { return GetComponent<LineRenderer>(); } }
+
+    #endregion
+
+    #region Public method
+
+    public void MakeLastNodeFixed()
+    {
+        LastNode.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        LastNode.GetComponent<HingeJoint2D>().connectedBody = null;
+    }
+
+    public void MakeLastNodeMobile()
+    {
+        LastNode.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        if (threadNodes.Count > 1)
+        {
+            LastNode.GetComponent<HingeJoint2D>().connectedBody = 
+                threadNodes[threadNodes.Count-2].GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+            LastNode.GetComponent<HingeJoint2D>().connectedBody =
+                attachedObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void RenderLine()
+    {
+        LineRenderer.numPositions = threadNodes.Count;
+
+        for (int i = 0; i < threadNodes.Count; i++)
+        {
+            LineRenderer.SetPosition(i, threadNodes[i].transform.position);
+        }
+    }
+
+    #endregion
+}
